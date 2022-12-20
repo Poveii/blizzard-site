3d0f8d9b3a97c9b17eb41a0467e4f59230d509e2
Poveii <yellowtutors@gmail.com>
2022-12-20 11:53:37 -0300

    :sparkles: feat: Created Footer Component

---
+++ src/Footer.jsx
@@ -0,0 +1,54 @@
+import "./Footer.css";
+
+function createDownloadButtonByOS(userAgent) {
+  const array = ["Linux", "Macintosh", "Windows"];
+
+  const actualOS = array.filter((item) => {
+    if (userAgent.includes(item)) return item;
+  })[0];
+
+  return (
+    <a href="">
+      <img src={`src/assets/${actualOS.toLowerCase()}-icon.svg`} alt="" />
+      Baixar para o {actualOS[0].toUpperCase() + actualOS.substring(1)}
+    </a>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <div className="container">
+        <img src="src/assets/battlenet-logo.svg" alt="" id="battleNetLogo" />
+        <h2>Baixe agora o battle.net</h2>
+        <ul>
+          <li>
+            <img src="src/assets/listitems-icon.svg" alt="" />
+            <p>Seus jogos em um só lugar</p>
+          </li>
+          <li>
+            <img src="src/assets/circles-icon.svg" alt="" />
+            <p>Conecte-se aos seus amigos</p>
+          </li>
+          <li>
+            <img src="src/assets/buy-icon.svg" alt="" />
+            <p>Compre jogos e itens digitais</p>
+          </li>
+        </ul>
+        {createDownloadButtonByOS(window.navigator.userAgent)}
+        <div>
+          <img src="src/assets/phone-icon.svg" alt="" />
+          <p>
+            Também disponível como <span>aplicativo móvel</span>
+          </p>
+        </div>
+      </div>
+
+      <div className="aside">
+        <img src="src/assets/battlenet-app-mini.png" alt="" id="presentation" />
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
