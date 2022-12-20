0b0ac8c98fa47df711e482e0e8a9ea291c27216b
Poveii <yellowtutors@gmail.com>
2022-12-20 11:39:16 -0300

    :sparkles: feat: Implemented BannerHero Component on App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import BannerHero from "./BannerHero";
 import Header from "./Header";
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <main>
+        <BannerHero></BannerHero>
+      </main>
     </div>
   );
 }
