a249d3cac1f15382d100fd1446cde6fb1ac688e2
Poveii <yellowtutors@gmail.com>
2022-12-28 23:17:35 -0300

    :sparkles: feat: Implemented ExclusiveGames Component on App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import BannerHero from "./BannerHero";
 import Header from "./Header";
 import Footer from "./Footer";
+import ExclusiveGames from "./ExclusiveGames";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <main>
         <BannerHero />
+        <ExclusiveGames />
       </main>
       <Footer />
     </div>
