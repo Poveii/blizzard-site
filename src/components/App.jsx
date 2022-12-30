d1275db99e379fc64c24a93a52d2c353eab494fb
Poveii <yellowtutors@gmail.com>
2022-12-30 15:38:36 -0300

    :sparkles: feat: Switching BannerHero with click on GamesLogo

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,20 @@
-import BannerHero from "./BannerHero";
+import React from "react";
+import BannerHero, { diabloInfoList } from "./BannerHero";
 import Header from "./Header";
 import Footer from "./Footer";
 import ExclusiveGames from "./ExclusiveGames";
 
 function App() {
+  const [infoGameList, setInfoGameList] = React.useState(diabloInfoList);
+
   return (
     <div className="App">
       <Header />
       <main>
-        <BannerHero />
+        <BannerHero
+          infoGameList={infoGameList}
+          setInfoGameList={setInfoGameList}
+        />
         <ExclusiveGames />
       </main>
       <Footer />
--- src/components/BannerHero.jsx
+++ src/components/BannerHero.jsx
@@ -1,23 +1,80 @@
 import "./BannerHero.css";
 
-function BannerHero() {
+export const diabloInfoList = {
+  header: "Retorne à escuridão com o game Diablo IV",
+  paragraph: "O retorno de Lilith traz uma era de escuridão e sofrimento",
+  cta: "Jogue agora",
+  name: "diablo-IV",
+};
+
+const hearthStoneInfoList = {
+  header: "Novo pacote de expansão de Hearthstone",
+  paragraph: "A Horda e a Aliança se encontraram no Vale Alterac para lutar",
+  cta: "Reserve agora na pré-venda",
+  name: "hearthstone",
+};
+
+const worldOfWarcraftInfoList = {
+  header: "Desbrave as Terras Sombrias em Shadowlands!",
+  paragraph: "O que jaz além do mundo que você conhece?",
+  cta: "Reserve agora na pré-venda",
+  name: "wow",
+};
+
+function BannerHero({ infoGameList, setInfoGameList }) {
   return (
-    <section id="bannerHero">
+    <section
+      id="bannerHero"
+      style={{
+        backgroundImage: `url(/assets/${infoGameList.name}-banner.png)`,
+      }}
+    >
       <div className="container">
         <div className="content">
-          <h1>Retorne à escuridão com o game Diablo IV</h1>
-          <p>O retorno de Lilith traz uma era de escuridão e sofrimento</p>
-          <a href="">Jogue agora</a>
+          <h1>{infoGameList.header}</h1>
+          <p>{infoGameList.paragraph}</p>
+          <a href="">{infoGameList.cta}</a>
         </div>
 
         <ul>
-          <li>
-            <img src="/assets/diablo-IV-icon.svg" alt="" />
+          <li
+            onClick={(e) => {
+              const wrapperParent = e.target.parentElement.parentElement;
+              wrapperParent.childNodes.forEach(
+                (item) => (item.firstChild.style.filter = "")
+              );
+              e.target.style.filter = "saturate(1)";
+              setInfoGameList(diabloInfoList);
+            }}
+          >
+            <img
+              src="/assets/diablo-IV-icon.svg"
+              alt=""
+              style={{ filter: "saturate(1)" }}
+            />
           </li>
-          <li>
+          <li
+            onClick={(e) => {
+              const wrapperParent = e.target.parentElement.parentElement;
+              wrapperParent.childNodes.forEach(
+                (item) => (item.firstChild.style.filter = "")
+              );
+              e.target.style.filter = "saturate(1)";
+              setInfoGameList(hearthStoneInfoList);
+            }}
+          >
             <img src="/assets/heartstone-icon.svg" alt="" />
           </li>
-          <li>
+          <li
+            onClick={(e) => {
+              const wrapperParent = e.target.parentElement.parentElement;
+              wrapperParent.childNodes.forEach(
+                (item) => (item.firstChild.style.filter = "")
+              );
+              e.target.style.filter = "saturate(1)";
+              setInfoGameList(worldOfWarcraftInfoList);
+            }}
+          >
             <img src="/assets/wow-icon.svg" alt="" />
           </li>
           <li>
@@ -30,13 +87,20 @@ function BannerHero() {
       </div>
 
       <div className="aside">
-        <img src="/assets/diablo-IV-logo.png" alt="" id="gameLogo" />
+        <img
+          src={`/assets/${infoGameList.name}-logo.png`}
+          alt=""
+          id="gameLogo"
+        />
 
         <div>
           <span>Assista ao Trailer</span>
           <div className="video">
             <img src="/assets/player-icon.svg" alt="" />
-            <img src="/assets/diablo-animation-cover.png" alt="" />
+            <img
+              src={`/assets/${infoGameList.name}-animation-cover.png`}
+              alt=""
+            />
           </div>
         </div>
       </div>
